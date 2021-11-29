3edf9a87997b325eaa03a65c8d9715e228131952
Levi Conall <levi.conall@gmail.com>
2021-11-28 22:58:28 -0700

    Finished the basic bowling game class

--- bowling_game.py
+++ bowling_game.py
@@ -3,10 +3,39 @@
 class BowlingGame():
     def __init__(self, *args, **kwargs):
         self._shots = []
+        self._current_shot = 0
 
     def add_shot(self, shot: int) -> None:
-        self._shots += [shot]
+        self._shots.append(shot)
 
     @property
     def score(self):
-        return sum(self._shots)
+        score = 0
+        frame_first_shot_index = 0
+        for frame in range(0, 10):
+            if self.is_strike(frame_first_shot_index):
+                score += self.strike_frame_score(frame_first_shot_index)
+                frame_first_shot_index += 1
+            elif self.is_spare(frame_first_shot_index):
+                score += self.spare_frame_score(frame_first_shot_index)
+                frame_first_shot_index += 2
+            else:
+                score += self.open_frame_score(frame_first_shot_index)
+                frame_first_shot_index += 2
+            print(f"frame={frame}, frame_first_shot_index={frame_first_shot_index}, score={score}")
+        return score
+
+    def is_strike(self, frame_first_shot_index):
+        return self._shots[frame_first_shot_index] == 10
+
+    def strike_frame_score(self, frame_first_shot_index):
+        return 10 + self._shots[frame_first_shot_index + 1] + self._shots[frame_first_shot_index + 2]
+
+    def is_spare(self, frame_first_shot_index):
+        return self._shots[frame_first_shot_index] + self._shots[frame_first_shot_index + 1] == 10
+
+    def spare_frame_score(self, frame_first_shot_index):
+        return 10 + self._shots[frame_first_shot_index + 2]
+
+    def open_frame_score(self, frame_first_shot_index):
+        return self._shots[frame_first_shot_index] + self._shots[frame_first_shot_index + 1]
--- tests/test_bowling_game.py
+++ tests/test_bowling_game.py
@@ -1,3 +1,5 @@
+import random
+
 
 from BowlingScoringApp.tests.pytest_base import PytestBase
 
@@ -12,26 +14,60 @@ class TestBowlingGame(PytestBase):
 
     def test_gutterGame(self):
         bowling_game = BowlingGame()
-
-        shots_per_frame = 2
-        number_of_frames = 10
+        number_of_frames, shots_per_frame = 2, 10
         total_shots = shots_per_frame * number_of_frames
-
-        shots = [0] * total_shots
-        for shot in shots:
-            bowling_game.add_shot(shot)
-
+        self.add_multiple_shots_with_same_value(bowling_game, 0, total_shots)
         assert bowling_game.score == 0
 
     def test_allRollsSinglePinsGame(self):
         bowling_game = BowlingGame()
-
-        shots_per_frame = 2
-        number_of_frames = 10
+        number_of_frames, shots_per_frame = 2, 10
         total_shots = shots_per_frame * number_of_frames
+        self.add_multiple_shots_with_same_value(bowling_game, 1, total_shots)
+        assert bowling_game.score == 20
 
-        shots = [1] * total_shots
+    def test_gameHasASpareFrame(self):
+        bowling_game = BowlingGame()
+        self.add_spare_frame(bowling_game)
+        bowling_game.add_shot(8)
+        bowling_game.add_shot(1)
+        number_of_frames, shots_per_frame = 2, 8
+        total_shots = shots_per_frame * number_of_frames
+        self.add_multiple_shots_with_same_value(bowling_game, 0, total_shots)
+        assert bowling_game.score == 27
+
+    def test_spareGame(self):
+        bowling_game = BowlingGame()
+        number_of_frames, shots_per_frame = 2, 10
+        total_shots = shots_per_frame * number_of_frames
+        self.add_multiple_shots_with_same_value(bowling_game, 5, total_shots)
+        bowling_game.add_shot(5)
+        assert bowling_game.score == 150
+
+    def test_gameHasAStrikeFrame(self):
+        bowling_game = BowlingGame()
+        bowling_game.add_shot(10)
+        bowling_game.add_shot(2)
+        bowling_game.add_shot(3)
+        number_of_frames, shots_per_frame = 2, 8
+        total_shots = shots_per_frame * number_of_frames
+        self.add_multiple_shots_with_same_value(bowling_game, 0, total_shots)
+        assert bowling_game.score == 20
+
+    def test_strikeGame(self):
+        bowling_game = BowlingGame()
+        number_of_frames, shots_per_frame = 2, 10
+        total_shots = shots_per_frame * number_of_frames
+        self.add_multiple_shots_with_same_value(bowling_game, 10, total_shots)
+        bowling_game.add_shot(10)
+        assert bowling_game.score == 300
+
+    def add_multiple_shots_with_same_value(self, bowling_game, shot_value: int, number_of_shots_to_add: int) -> None:
+        shots = [shot_value] * number_of_shots_to_add
         for shot in shots:
             bowling_game.add_shot(shot)
 
-        assert bowling_game.score == 20
+    def add_spare_frame(self, bowling_game):
+        first_shot = random.randint(0, 9)
+        bowling_game.add_shot(first_shot)
+        bowling_game.add_shot(10 - first_shot)
